0249fed4d04d6c45879fba5637e68260df108e7f
Tamschi <MailTamschi@googlemail.com>
2019-02-12 15:12:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using TQ.Animation;
+
+namespace TQ.Animation_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Span<byte> file = File.ReadAllBytes("animation.anm");
+            var animation = new Animation.Animation(file);
+            Console.WriteLine($"File format version: {animation.Header.Version}");
+            Console.WriteLine($"Bone Count: {animation.Header.BoneCount}");
+            Console.WriteLine($"Frame Count: {animation.Header.FrameCount}");
+            Console.WriteLine($"FPS: {animation.Header.Fps}");
+
+            var enumerator = animation.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var boneAnimation = enumerator.Current;
+                Console.WriteLine($"Bone animation: {boneAnimation.Name}");
+                foreach (ref var frame in boneAnimation.Frames)
+                { Console.WriteLine(frame); }
+            }
+            Console.WriteLine("=== Text Data ===");
+            Console.WriteLine(enumerator.TextAfterEnd);
+        }
+    }
+}
